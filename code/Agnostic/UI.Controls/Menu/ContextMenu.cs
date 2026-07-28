e44db1500756855c9dc7f44c5fe9751d5e99e463
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2026-07-28 11:58:25 -0400

    Code: Menu simplification

--- code/Agnostic/UI.Controls/Menu/ContextMenu.cs
+++ code/Agnostic/UI.Controls/Menu/ContextMenu.cs
@@ -1,20 +1,8 @@
 namespace SA.Agnostic.UI.Controls;
 
 public class ContextMenu : System.Windows.Controls.ContextMenu {
-<<<<<<< HEAD
     public ContextMenu() =>
         Nicety = Nicety.ResouceInitializer<ContextMenu>.Setup(this);
     public Nicety Nicety { set => SetValue(Nicety.ContextMenuNicetyProperty, value); }
     public static bool IsContextMenu { get => true; }   
-=======
-
-    public ContextMenu() {
-        Nicety = Nicety.Initialize();
-        this.Resources.MergedDictionaries.Add(MenuResourceHost.Instance.Resources);
-    } //ContextMenu
-
-    public Nicety Nicety { set => SetValue(Nicety.ContextMenuNicetyProperty, value); }
-    public static bool IsContextMenu { get => true; }
-    
->>>>>>> 6626abca9b7f4f0002f175868e7a0bc4a0300453
 } //ContextMenu
--- code/Agnostic/UI.Controls/Menu/Menu.cs
+++ code/Agnostic/UI.Controls/Menu/Menu.cs
@@ -1,20 +1,8 @@
 namespace SA.Agnostic.UI.Controls;
 
 public class Menu : System.Windows.Controls.Menu {
-<<<<<<< HEAD
     public Menu() =>
         Nicety = Nicety.ResouceInitializer<Menu>.Setup(this);
     public Nicety Nicety { set => SetValue(Nicety.MenuNicetyProperty, value); }
-    public static bool IsContextMenu { get => false; }  
-=======
-
-    public Menu() {
-        Nicety = Nicety.Initialize();
-        Resources.MergedDictionaries.Add(MenuResourceHost.Instance.Resources);
-    } //Menu
-
-    public Nicety Nicety { set => SetValue(Nicety.MenuNicetyProperty, value); }
-    public static bool IsContextMenu { get => false; }
-    
->>>>>>> 6626abca9b7f4f0002f175868e7a0bc4a0300453
+    public static bool IsContextMenu { get => false; }   
 } //Menu
